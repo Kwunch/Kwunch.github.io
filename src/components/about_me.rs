13bbd7c8b72fa95014a622a3b76eae9243caa709
T1buR0n <95244424+Kwunch@users.noreply.github.com>
2025-07-31 19:29:38 -0400

    Update about_me.rs

--- src/components/about_me.rs
+++ src/components/about_me.rs
@@ -10,31 +10,16 @@ pub(crate) fn AboutMe() -> Html {
                 <div class="col">
                         <section class = "sub">
                         <h2 id="unders">{"Who am I?"}</h2>
-                        <h4><p>{"I am a student at the University of Pittsburgh pursuing as BS in \
-                        Computer Science, a BS in Quantum Computing and Physics, and a BA in Mathematics. \
-                        My anticipated graduation date is Summer 2025. With my three degrees my primary pursuit \
-                        is Software Engineering with a focus on Systems and Systems Design, \
-                        however, I'm open to all opportunities. I have a mastery in multiple programming languages, \
-                        which can be referenced in the Resume section. Rust is my preferred language for \
-                        professional and personal applications. I pride myself in my work on programming languages and coding, as well as \
-                        understanding the underlying concepts on the creation and design of languages."}</p></h4>
+                        <h4><p>{"TBD"}</p></h4>
                     </section>
                 </div>
                 // Right side of the page
                 <div class="col">
                     <section class = "sub">
                         <h2 id="unders">{"What are my interests?"}</h2>
-                        <h4><p>{"Programming is not just career pursuit for me, it is also one of my primary hobbies. \
-                         I am consistently expanding my GitHub with new projects and ideas. My pique interests now \
-                         involve System Design including embedded systems and logic controllers, \
-                         as well as encryption and compression algorithms. \
-                         I have done projects working on Arduino, Raspberry Pi and numerous other logic controllers. \
-                         I have also done projects working with the Linux Kernel. "}</p></h4>
+                        <h4><p>{"TBD"}</p></h4>
 
-                        <h4><p>{" While most of my projects may be in C or Rust, I have a firm understanding
-                        of OOP languages such as Java, Ruby, and Python. I also have sufficient experience doing web development
-                        or front end development in languages such as React. "} </p></h4>
-
+                        <h4><p>{"TBD"} </p></h4>
                     </section>
                 </div>
             </section>
