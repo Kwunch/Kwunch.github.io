f10094ce2136a2b3ea1f0be708645809f4a14bc2
T1buR0n <95244424+Kwunch@users.noreply.github.com>
2025-07-31 19:39:51 -0400

    Update contacts.rs

--- src/components/contacts.rs
+++ src/components/contacts.rs
@@ -12,26 +12,8 @@ pub(crate) fn Contact() -> Html {
                   <ul class="about separate">
                     <li>
                       <ul>
-                        <li><h4>{"Professional Development"}</h4></li>
-                        <li><a href="mailto:kwunch_devs.moqsz@passmail.net" id="emails">
-                            {"kwunch_devs.moqsz@passmail.net"}</a></li>
-                      </ul>
-                    </li>
-                    <li>
-                      <ul>
-                        <li><h4>{"General Inquiries"}</h4></li>
-                        <li><a href="mailto:Kwunch@outlook.com" id="emails">
-                            {"Kwunch@outlook.com"}</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </section>
-                <section class="sub">
-                  <h3>{"Phone"}</h3>
-                  <ul class="about separate">
-                    <li>
-                      <ul>
-                        <li>{"+1 (412)-606-4704"}</li>
+                        <li><a href="mailto:t1bur0n@proton.me" id="emails">
+                            {"t1bur0n@proton.me"}</a></li>
                       </ul>
                     </li>
                   </ul>
